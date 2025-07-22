142cb3f75ef3573656c38408e9fdef42e7dce443
lokesh mangamuri <mangamurilokesh4@gmail.com>
2025-07-22 23:18:16 +0200

    Fix: bind Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
@@ -31,4 +32,5 @@ def idpage():
     return render_template('idpage.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))  # Render sets PORT env variable
+    app.run(host='0.0.0.0', port=port, debug=True)
